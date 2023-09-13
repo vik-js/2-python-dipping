9a4b7da9aed1e9c75dd11c35c6284d9b620f765f
Viktoriya Kosteckaya <20206828+vik-js@users.noreply.github.com>
2023-09-13 21:17:24 +0500

    Решение задачи 9

---
+++ seminar-1/Task09.py
@@ -0,0 +1,20 @@
+# Задание №9
+# Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
+
+for j in range(2, 11):
+    for i in range(2, 10):
+        if i < 6:
+            if j == 10:
+                print(f'{i:>0} x {j} = {i * j : <9}', end='')
+            else:
+                print(f'{i} x {j} = {i * j : <10}', end='')
+    print('')
+print('')
+for j in range(2, 11):
+    for i in range(2, 10):
+        if i > 5:
+            if j == 10:
+                print(f'{i:>0} x {j} = {i * j : <9}', end='')
+            else:
+                print(f'{i} x {j} = {i * j : <10}', end='')
+    print('')
