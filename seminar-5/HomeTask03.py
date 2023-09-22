86bf2bc111ad2ed55d09f5b306b0aa9d0f96018e
Viktoriya Kosteckaya <20206828+vik-js@users.noreply.github.com>
2023-09-22 21:55:02 +0500

    Вывод только размера премии, вместо полной выплаты с премией

--- seminar-5/HomeTask03.py
+++ seminar-5/HomeTask03.py
@@ -9,5 +9,6 @@ lst1 = ['Александра', 'Виктория', 'Вероника']
 lst2 = [10_000, 20_000, 5_000]
 lst3 = ['10.25%', '20.11%', '5.75%']
 
-dictionary = {l1: l2 * float(l3[:-1]) / 100 + l2 for l1, l2, l3 in zip(lst1, lst2, lst3)}
+dictionary = {l1: l2 * float(l3[:-1]) / 100 for l1, l2, l3 in zip(lst1, lst2, lst3)}  # размер премии
+#dictionary = {l1: l2 * float(l3[:-1]) / 100 + l2 for l1, l2, l3 in zip(lst1, lst2, lst3)}  # полная сумма с премией
 print(dictionary)
